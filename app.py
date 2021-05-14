c5b420274e809a3ac2ad4bfdc0b89f043d946d17
Fabien Droux <fabien.droux@gmail.com>
2021-05-14 23:37:52 +0200

    Ajout class db

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://gearsports.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+db = SQLAlchemy(app)
+
+
+class Gears(db.Model):
+    gear_id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    description = db.Column(db.String(50), unique=False, index=True)
+    purchase_date = db.Column(db.Date, default=datetime.now())
+    offset = db.Column(db.Integer(), nullable=False, unique=False, default=0)
+
+
+class Workout(db.Model):
+    workout_id = db.Column(db.Integer(), primary_key=True)
+    sport_id = db.Column(db.String(50))  # A modifier par relation
+    workout_date = db.Column(db.Date, default=datetime.now())
+    distance = db.Column(db.Float(), default=0)
+    avg_pace = db.Column(db.Float(), default=0)
+    avg_fc = db.Column(db.Float(), default=0)
+
+
+class Sports(db.Model):
+    sport_id = db.Column(db.Integer(), primary_key=True)
+    sport = db.Column(db.String(50), index=True, nullable=False, unique=True)
 
 
 @app.route('/')
