784333e92714086dd402510ec98ab8dccd2ae9dd
Fabien Droux <fabien.droux@gmail.com>
2021-05-22 23:55:23 +0200

    Update routes and add sport form + db

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gearsports.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'MySupeRsEcretkeY'
 
 db = SQLAlchemy(app)
+
+
+class SportForm(FlaskForm):
+    sport = StringField(label='Sport :', validators=[DataRequired()])
+    submit = SubmitField('Add Sport')
 
 
 class Gear(db.Model):
@@ -15,7 +24,7 @@ class Gear(db.Model):
     description = db.Column(db.String(50), unique=False, index=True)
     purchase_date = db.Column(db.Date, default=datetime.now())
     offset = db.Column(db.Integer(), nullable=False, unique=False, default=0)
-    tracks = db.relationship('Track', backref='track', lazy='dynamic')
+    tracks = db.relationship('Track', backref='gear_track', lazy='dynamic')
 
 
 class Workout(db.Model):
@@ -25,7 +34,7 @@ class Workout(db.Model):
     distance = db.Column(db.Float(), default=0)
     avg_pace = db.Column(db.Float(), default=0)
     avg_fc = db.Column(db.Float(), default=0)
-    tracks = db.relationship('Track', backref='track', lazy='dynamic')
+    tracks = db.relationship('Track', backref='workout_track', lazy='dynamic')
 
 
 class Sport(db.Model):
@@ -40,8 +49,30 @@ class Track(db.Model):
 
 
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+def index():
+    return render_template("index.html", page_home_active="active")
+
+
+@app.route('/sports', methods=['GET', 'POST'])
+def sports():
+
+    create_sport_form = SportForm()
+
+    if request.method == 'POST' and create_sport_form.validate():
+        new_sport = Sport(sport=create_sport_form.sport.data)
+        db.session.add(new_sport)
+        db.session.commit()
+    else:
+        flash(create_sport_form.errors)
+
+    sports_list = Sport.query.all()
+    return render_template('sports.html', sports_list=sports_list, page_sports_active="active", create_sport_form=create_sport_form)
+
+
+@app.route('/gears')
+def gears():
+    gears_list = Gear.query.all()
+    return render_template('gears.html', gears_list=gears_list, page_gears_active="active")
 
 
 if __name__ == '__main__':
